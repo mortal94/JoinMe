0e53a9496825d1617a838da7eaad11ffbef55eec
Nadav Kaner <nadav@whisp.software>
2018-05-23 18:57:23 +0300

    remove log

--- components/MeetingsList.js
+++ components/MeetingsList.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import { View, ScrollView } from 'react-native';
-import { RkText } from 'react-native-ui-kitten';
 import FirebaseGateway from '../utils/firebase';
-import MeetingComponent from "./MeetingComponent";
+import MeetingComponent from './MeetingComponent';
 
 export default class MeetingsList extends Component {
   componentDidMount() {
     FirebaseGateway.getAllMeetings(snapshot => {
       if (snapshot.exists()) {
         const meetings = snapshot.val();
-        console.log(meetings);
-        this.setState({ meetings })
+        this.setState({ meetings });
       }
-    })
+    });
   }
 
   render() {
     return (
       <View>
         <ScrollView>
-          <MeetingComponent/>
+          <MeetingComponent />
         </ScrollView>
       </View>
-    )
+    );
   }
 }
